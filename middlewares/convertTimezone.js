f328fac537451dd6ebf3610a94657bb5ed343776
YugendarM <yugendar2411@gmail.com>
2024-05-08 10:16:36 +0530

    Add - added middleware for timeformat conversion

---
+++ middlewares/convertTimezone.js
@@ -0,0 +1,33 @@
+const moment = require("moment-timezone")
+const timezoneModel = require("../model/timezoneModel")
+
+let utcConvertedStart = ""
+let utcConvertedEnd = ""
+const convertUtcTimezone = (startDate, endDate, timezone) => { 
+    utcConvertedStart = moment.tz(startDate,"YYYY-MM-DDTHH:mm:ss", timezone ).utc().format("DD-MM-YYYY HH:mm")
+    utcConvertedEnd = moment.tz(endDate, "YYYY-MM-DDTHH:mm:ss", timezone).utc().format("DD-MM-YYYY HH:mm")
+    console.log(utcConvertedStart, utcConvertedEnd);
+}
+
+const convertTimeZone = (start, end, timezone) => {
+    let convertedStart = moment.tz(start, timezone).format('DD-MM-YYYY HH:mm');
+    let convertedEnd = moment.tz(end, timezone).format('DD-MM-YYYY HH:mm');
+    return { convertedStart, convertedEnd };
+}
+
+const convertTimezoneMiddleware = async(request, response) => {
+    const userData =  await timezoneModel.findOne().sort({createdAt: -1})
+    try{
+        convertUtcTimezone(userData.startDate, userData.endDate, userData.timezone)
+        const finalConvertedIST = convertTimeZone(utcConvertedStart, utcConvertedEnd, "Asia/Kolkata")
+        const finalConvertedJST = convertTimeZone(utcConvertedStart, utcConvertedEnd, "Asia/Tokyo")
+        const finalConvertedCMT = convertTimeZone(utcConvertedStart, utcConvertedEnd, "Asia/Kolkata")
+        const finalConvertedPacific = convertTimeZone(utcConvertedStart, utcConvertedEnd, "America/Los_Angeles")
+        response.status(200).send({IST: finalConvertedIST, JST: finalConvertedJST, CMT: finalConvertedCMT, Pacific: finalConvertedPacific})
+    }
+    catch(error){
+        response.status(500).send({message: error.message})
+    }
+}
+
+module.exports = convertTimezoneMiddleware
--- router/timezoneRouter.js
+++ router/timezoneRouter.js
@@ -1,9 +1,14 @@
 const express = require("express")
 const route = express()
 const {getAllEntry, addNewEntry, getLatestEntry} = require("../controller/timezoneController")
+const convertTimezoneMiddleware = require("../middlewares/convertTimezone")
 
 route.get('/', getAllEntry)
 route.post('/add', addNewEntry)
-route.get('/latest_entry', getLatestEntry)
+route.get('/latest_entry', convertTimezoneMiddleware,  getLatestEntry)
+
+// route.param
+
+
 
 module.exports = route
